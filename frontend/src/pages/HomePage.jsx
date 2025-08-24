9fa9bd1a9ac263bfa0be3d8e33bbc6138b664694
AnilDevchhetri <chhetrivision@gmail.com>
2025-08-24 22:00:15 +0900

    2:01

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <button className="btn btn-outline">Click me</button>
+            HomePage</div>
+    )
+}
+
+export default HomePage
